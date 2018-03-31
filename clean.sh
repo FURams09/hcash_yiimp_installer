c077346e9c92cb63187839104948844c3625a98b
Greg Padin <padin.greg@gmail.com>
2018-03-30 22:38:43 -0400

    fixed stratum typo

--- clean.sh
+++ clean.sh
@@ -1,7 +1,7 @@
 read -e -p "Server name to remove from var/www: " server_name
 
 sudo rm -rf $HOME/HCash_yiimp
-sudo rm -rf /var/stratus
+sudo rm -rf /var/stratum
 sudo rm -rf /var/web
 sudo rm -rf /etc/yiimp
 sudo rm -rf /bin/yiimp
