5af96a5b7ad7a5d93544d52e3c7024e2245add30
Greg Padin <padin.greg@gmail.com>
2018-03-31 10:54:33 -0400

    removing sites-enabled in clean

--- clean.sh
+++ clean.sh
@@ -7,7 +7,7 @@ sudo rm -rf /etc/yiimp
 sudo rm -rf /bin/yiimp
 sudo rm -rf /var/www/$server_name
 sudo rm -rf /etc/nginx/sites-available/$server_name.conf
-
+sudo rm -rf /etc/nginx/sites-enabled/$server_name.conf
 
 
 echo 'Clean Complete. Go ahead and reinstall HCash_yiimp'
